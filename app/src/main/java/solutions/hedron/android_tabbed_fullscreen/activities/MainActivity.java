c8c15af612bc0ddafe7724cbad0cf1d252c4e37b
Armin <armin.ag@outlook.com>
2017-03-02 11:06:51 +0100

    Add Fragments for each Tab

--- app/src/main/java/solutions/hedron/android_tabbed_fullscreen/activities/MainActivity.java
+++ app/src/main/java/solutions/hedron/android_tabbed_fullscreen/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package solutions.hedron.android_tabbed_fullscreen;
+package solutions.hedron.android_tabbed_fullscreen.activities;
 
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -18,6 +18,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+
+import solutions.hedron.android_tabbed_fullscreen.R;
+import solutions.hedron.android_tabbed_fullscreen.fragments.FeedActivityFragment;
+import solutions.hedron.android_tabbed_fullscreen.fragments.HomeFragment;
+import solutions.hedron.android_tabbed_fullscreen.fragments.ProfileFragment;
+import solutions.hedron.android_tabbed_fullscreen.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -89,41 +95,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -135,26 +106,34 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position){
+                case 0:
+                    return HomeFragment.newInstance();
+                case 1:
+                    return SearchFragment.newInstance();
+                case 2:
+                    return FeedActivityFragment.newInstance();
+                default:
+                    return ProfileFragment.newInstance();
+            }
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return 4;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "HOME";
                 case 1:
-                    return "SECTION 2";
+                    return "SEARCH";
                 case 2:
-                    return "SECTION 3";
+                    return "ACTIVITY";
+                case 3:
+                    return "PROFILE";
             }
             return null;
         }
